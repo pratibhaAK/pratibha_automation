a884d876f970823e9440e9908e3ccf0280edf576
Pratibha <Pratibha.andukuri@gmail.com>
2019-12-16 15:22:45 +0530

    added  Page factory for search

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
@@ -25,6 +26,9 @@ public class HomePage extends SuiteManager {
     @FindBy(id="keywords")
     private WebElement keywords;
 
+    @FindAll(@FindBy(xpath="//div[@data-hook='products_list_item']"))
+    private List<WebElement> numberOfItems;
+
     public void searchProduct(){
         keywords.sendKeys(searchItem());
         keywords.sendKeys(Keys.ENTER);
@@ -33,7 +37,7 @@ public class HomePage extends SuiteManager {
 
     public List getItems() {
         List<WebElement> webElementList = new ArrayList();
-        webElementList = DriverManager.driver.findElements(By.xpath(numberOfProducts));
+        webElementList = numberOfItems;
         int listSize = webElementList.size();
         return webElementList;
     }
